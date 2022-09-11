0b8126376bb3d7b82d20d4e9d1c9140c762770e3
xdgonex <bendezucortezjoseluis@gmail.com>
2022-09-11 17:51:32 -0500

    Validacion al no ingresar datos al Tweet

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
+//VARIABLES
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
 
+eventListener();
+
+//EVENTOS
+
+function eventListener()
+{
+    formulario.addEventListener('submit',agregarTweet);
+}
+
+
+//FUNCIONES
+
+function agregarTweet(e)
+{
+    e.preventDefault();
+
+    const tweet = document.querySelector('#tweet').value;
+    
+    if(tweet === '')
+    {
+        mostrarError('Un mensaje no puede ir vacio');
+        return; //RETURN EN UN IF, CANCELA TODA OPERACION DETENIENDOLA, FUNCIONA CUANDO ESTA DENTRO DE UNA FUNCION
+    }
+
+    console.log(tweet);
+}
+
+function mostrarError(mensaje)
+{
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('error');
+
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    //EL MENSAJE SE BORRE EN 3 SEGUNDOS
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
+}
